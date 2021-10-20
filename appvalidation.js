9f010e4ae69fa0d4b0ea965245b8de383f51b53a
Harsh5itha <45786603+Harsh5itha@users.noreply.github.com>
2021-10-21 03:52:54 +0530

    Add validation while creating DB

---
+++ appvalidation.js
@@ -0,0 +1,45 @@
+//jshint esversion:6
+//Run Server
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/fruitsdb",{useNewUrlParser:true});
+
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true,'Fruit Name is required']
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  review: String
+});
+const Fruit = mongoose.model("Fruit", fruitSchema);
+const fruit = new Fruit ({
+  rate: 6,
+  review: "Keeps Doctor Away"
+});
+fruit.save();
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+const Person = mongoose.model("Person", personSchema);
+const person = new Person({
+  name: "Harshitha",
+  age: 23
+});
+// person.save();
+
+Fruit.find(function(err, fruits){
+  if(err) {
+    console.log(err);
+  } else {
+    // console.log(fruits);
+    mongoose.connection.close();
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    });
+  }
+});
